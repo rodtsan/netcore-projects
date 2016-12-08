f8c33893f71ae31f0d2fffabc5d8ea20c5a92626
Rodrigo Santos <rodtsan@hotmail.com>
2016-12-08 09:30:17 -0600

    Revert "Initial Commit"
    This reverts commit db4f7d013d89805a8ee554a5016431d5c39a2f9f.

--- lib/PinoyCode.Domain.Identity/Models/UserLogin.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using System;
-
-namespace PinoyCode.Domain.Identity.Models
-{
-    public class UserLogin : IdentityUserLogin<Guid>
-    {
-    }
-}
